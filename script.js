49fd7315e04f81d03362ce710de1cb812e786184
GiancarloconteDiTremonte <tulipanileonardo@gmail.com>
2026-03-22 21:25:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function mostraForza() {
+    document.getElementById("descrizione").innerHTML =
+        "Le armi Forza infliggono danni enormi ma sono più lente e scalano con la statistica FORZA. Sono caratterizzate da uno stile di gioco paziente ma devastante quando si colpisce." 
+}
+
+function mostraDestrezza() {
+    document.getElementById("descrizione").innerHTML =
+        "Le armi Destrezza colpiscono molto velocemente e consumano meno stamina e scalano con la statistica DESTREZZA. Sono caratterizzate da uno stile di gioco rapido, fatto di schivate e contrattacchi.";
+}
+
+function mostraIntelligenza() {
+    document.getElementById("descrizione").innerHTML =
+        "Le build Intelligenza utilizzano magie devastanti e armi magiche scalate con INTELLIGENZA. Sono caratterizzate da uno stile di gioco più distante dal nemico.";
+}
+
+function mostraFede() {
+    document.getElementById("descrizione").innerHTML =
+        "Le build Fede sfruttano miracoli e danni sacri che aumentano il loro danno con l'aumentare della statistca FEDE. La maggior parte degi incantesimi si utilizzano a distanza, ma c'è anche una grande varietà di incantesimi che permettono un approccio più ravvicinato.";
+}
